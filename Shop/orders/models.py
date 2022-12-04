037141536b0971aa1431aeca00ed99da43705039
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-04 18:12:38 +0330

    Shop | orders | models.py | Created Order model & OrderItem model

--- Shop/orders/models.py
+++ Shop/orders/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from product.models import Product
+from customers.models import Customer
 
-# Create your models here.
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.PositiveIntegerField(blank=True, null=True, default=None)
+
+    class Meta:
+        ordering = ('paid', '-updated')
+
+    def __str__(self):
+        return f'{self.customer} - {self.id}'
+
+    def get_total_price(self):
+        total = sum(item.get_cost() for item in self.items.all())
+        if self.discount:
+            discount_price = total * self.discount / 100
+            return int(total - discount_price)
+        return total
+
+
+class OrderItem(models):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
