96f646f98e0c501fd280ddf322b1fb0e31c91aa7
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-04 17:49:03 +0330

    Shop | Shop | settings.py | Add installed apps & AUTH_USER_MODEL

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -29,6 +29,13 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    'core.apps.CoreConfig',
+    'customers.apps.CustomersConfig',
+    'product.apps.ProductConfig',
+    'orders.apps.OrdersConfig',
+]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
