b465f92931d467192bb7b70e267874bd1687a068
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-04 17:11:18 +0330

    Shop | Core | forms.py | Create UserCreationForm & UserChangeForm for admin panel

---
+++ Shop/core/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'full_name')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('Passwords don\'t match')
+        return cd['password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        help_text="You can change password using <a href=\"../password/\">this form</a>")
+
+    class Meta:
+        model = User
+        fields = ['email', 'phone_number', 'full_name', 'password', 'last_login']
