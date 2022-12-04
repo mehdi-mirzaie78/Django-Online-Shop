bf1423f4f0936028856372c6c84138075735f019
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-04 21:15:49 +0330

    Shop | customers | models.py | Modify Customer model and Address model

--- Shop/customers/models.py
+++ Shop/customers/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from core.models import User
 
 
-class Customer(User):
-    is_customer = models.BooleanField(default=True)
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.full_name if self.user.full_name else self.user.phone_number
 
 
 class Address(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses')
     city = models.CharField(max_length=20)
     body = models.CharField(max_length=120)
+    postal_code = models.CharField(max_length=10)
 
     def __str__(self):
         return f'{self.city} - {self.body}'
