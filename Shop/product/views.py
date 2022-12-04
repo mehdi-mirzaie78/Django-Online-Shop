2505e302e81ffebd439d054e5be306b2e69172f3
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-05 00:31:09 +0330

    Shop | product | views.py | Create view of HomeView

--- Shop/product/views.py
+++ Shop/product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'product/index.html', {'products': products})
