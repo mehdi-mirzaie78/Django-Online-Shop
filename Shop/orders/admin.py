8156747a6ba054678d8098e227a7633a23d90e22
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-04 21:17:30 +0330

    Shop | orders | admin.py | Register models in admin panel

--- Shop/orders/admin.py
+++ Shop/orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Order, OrderItem, Coupon
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Coupon)
