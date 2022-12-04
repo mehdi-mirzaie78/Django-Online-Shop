dbf0f98133fb8c758dc6857e13176fe234ffdb12
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-04 17:35:57 +0330

    Shop | customers | models.py | Created Customer model & Address model

--- Shop/customers/models.py
+++ Shop/customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+
+class Customer(User):
+    is_customer = models.BooleanField(default=True)
+
+
+class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses')
+    city = models.CharField(max_length=20)
+    body = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f'{self.city} - {self.body}'
