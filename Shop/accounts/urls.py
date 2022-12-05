55ded361fa4923b4a49750b7854ddfdea5059a8c
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-06 00:43:18 +0330

    Shop | accounts | urls.py | Moved from core to accounts

--- Shop/accounts/urls.py
+++ Shop/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'core'
+app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('verify/', views.UserRegisterVerifyCodeView.as_view(), name='verify_code'),
