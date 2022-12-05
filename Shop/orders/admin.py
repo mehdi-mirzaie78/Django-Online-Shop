ad9f12e15b7b6e8201931da59fd2268e7dd74c31
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-06 00:54:18 +0330

    Shop | orders | admin.py | Customized admin panel

--- Shop/orders/admin.py
+++ Shop/orders/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Order, Coupon
 
-admin.site.register(Order)
-admin.site.register(Coupon)
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('discount', 'valid_since', 'valid_until', 'active')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'paid')
+    list_filter = ('paid', 'customer')
+    search_fields = ('customer',)
