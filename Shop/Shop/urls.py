795f21f3688a36312ab4c1c11bd060c15d912ba0
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-06 00:42:42 +0330

    Shop | Shop | urls.py | Changed urls from core to accounts

--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls', namespace='product')),
-    path('core/', include('core.urls', namespace='core')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
