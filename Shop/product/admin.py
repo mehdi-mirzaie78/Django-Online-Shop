8d0fa737e7fa3992ebbf3b45916bb6714b5ef685
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-06 00:53:52 +0330

    Shop | product | admin.py | Customized admin panel

--- Shop/product/admin.py
+++ Shop/product/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Product, Category, Comment
 
-admin.site.register(Product)
 admin.site.register(Category)
-admin.site.register(Comment)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    raw_id_fields = ('category',)
+    list_display = ('name', 'price', 'stock', 'is_available')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'product', 'title')
+    list_filter = ('customer', 'product')
+    search_fields = ('customer', 'product')
