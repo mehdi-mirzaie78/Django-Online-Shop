d1acedf16bc436ae4c7dd8e98b6bd53a7e23757e
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-08 00:55:38 +0330

    Shop | product | views.py | Added ProductDetailsView

--- Shop/product/views.py
+++ Shop/product/views.py
@@ -7,3 +7,10 @@ class HomeView(View):
     def get(self, request):
         products = Product.objects.all()
         return render(request, 'product/index.html', {'products': products})
+
+
+class ProductDetailsView(View):
+    def get(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        properties = product.properties.all()
+        return render(request, 'product/details.html', {'product': product, 'properties': properties})
