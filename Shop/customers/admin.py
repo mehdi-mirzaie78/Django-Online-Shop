c66bd967a0379f703a3ac48951393ebf3b0e64f3
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-09 13:21:52 +0330

    Shop | customers | admin.py | Modified admin panel

--- Shop/customers/admin.py
+++ Shop/customers/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from .models import Customer, Address
+from core.admin import BaseAdmin
 
-admin.site.register(Customer)
-admin.site.register(Address)
+
+class AddressInline(admin.TabularInline):
+    model = Address
+
+
+@admin.register(Customer)
+class CustomerAdmin(BaseAdmin):
+    ordering = ('user',)
+    list_display = ('user',)
+    search_fields = ('user_full_name', 'user_phone_number', 'user_email')
+
+    fieldsets = (
+        ('Personal Information', {'fields': ('user',)}),
+        ('Optional', {'fields': ('image',)})
+    )
+
+    inlines = (AddressInline,)
+
+
+@admin.register(Address)
+class AddressAdmin(BaseAdmin):
+    ordering = ('city',)
+    list_display = ('customer', 'city', 'body', 'postal_code')
+    list_editable = ('city', 'body')
+    list_filter = ('city', 'body')
+
+# admin.site.register(Customer, CustomerAdmin)
+# admin.site.register(Address, AddressAdmin)
