bfee40f89d004f0b1e41dff0516e34ad1dab8377
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-09 13:22:46 +0330

    Shop | orders | admin.py | Modified admin panel

--- Shop/orders/admin.py
+++ Shop/orders/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from .models import Order, Coupon
+from .models import Order, OrderItem, Coupon
+from core.admin import BaseAdmin
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
 
 
 @admin.register(Coupon)
-class CouponAdmin(admin.ModelAdmin):
+class CouponAdmin(BaseAdmin):
     list_display = ('discount', 'valid_since', 'valid_until', 'active')
 
 
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
+class OrderAdmin(BaseAdmin):
     list_display = ('id', 'customer', 'paid')
     list_filter = ('paid', 'customer')
     search_fields = ('customer',)
+    inlines = (OrderItemInline,)
