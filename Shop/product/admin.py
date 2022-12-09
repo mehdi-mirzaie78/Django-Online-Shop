800e27f67dbde2ef41de38f6637618f295e4ac07
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-09 13:25:46 +0330

    Shop | product | admin.py | Modified admin panel

--- Shop/product/admin.py
+++ Shop/product/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
 from .models import Product, Category, Comment, Property
+from core.admin import BaseAdmin
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(BaseAdmin):
+    list_display = ('name', 'sub_category', 'is_sub')
     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
+class PropertyAdmin(BaseAdmin):
     raw_id_fields = ('product',)
     list_display = ('key', 'value')
     list_filter = ('key', 'value')
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(BaseAdmin):
     prepopulated_fields = {'slug': ('name',)}
     raw_id_fields = ('category',)
     list_display = ('name', 'price', 'stock', 'is_available')
@@ -24,7 +26,7 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(BaseAdmin):
     list_display = ('customer', 'product', 'title')
     list_filter = ('customer', 'product')
     search_fields = ('customer', 'product')
