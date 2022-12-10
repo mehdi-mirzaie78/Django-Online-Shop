64c39bbdff1bb08b99ce74e00cbb42b410008fa1
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-10 13:31:22 +0330

    Shop | product | admin.py | Customized admin panel

--- Shop/product/admin.py
+++ Shop/product/admin.py
@@ -5,14 +5,14 @@ from core.admin import BaseAdmin
 
 @admin.register(Category)
 class CategoryAdmin(BaseAdmin):
-    list_display = ('name', 'sub_category', 'is_sub')
+    list_display = ('name', 'sub_category', 'is_sub', 'created', 'updated', 'is_deleted')
     prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Property)
 class PropertyAdmin(BaseAdmin):
     raw_id_fields = ('product',)
-    list_display = ('key', 'value')
+    list_display = ('key', 'value', 'created', 'updated', 'is_deleted')
     list_filter = ('key', 'value')
 
 
@@ -20,13 +20,16 @@ class PropertyAdmin(BaseAdmin):
 class ProductAdmin(BaseAdmin):
     prepopulated_fields = {'slug': ('name',)}
     raw_id_fields = ('category',)
-    list_display = ('name', 'price', 'stock', 'is_available')
-    list_filter = ('name', 'price', 'stock',)
+    list_display = ('name', 'price', 'stock', 'is_available', 'created', 'updated', 'is_deleted', 'is_active')
+    list_filter = ('category', 'name', 'price', 'stock',)
     search_fields = ('name',)
+    ordering = ('is_deleted', 'name')
+    list_per_page = 10
 
 
 @admin.register(Comment)
 class CommentAdmin(BaseAdmin):
-    list_display = ('customer', 'product', 'title')
+    list_display = ('customer', 'product', 'title', 'created', 'updated', 'is_deleted', 'is_active')
     list_filter = ('customer', 'product')
     search_fields = ('customer', 'product')
+    list_per_page = 10
