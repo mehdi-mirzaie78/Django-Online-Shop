5e002473be5cab09681962157f48b16062c03420
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-10 20:19:06 +0330

    Shop | orders | models.py | deleted Receipt & Add city and body for addresses to Order model & regex validator for postal code & more

--- Shop/orders/models.py
+++ Shop/orders/models.py
@@ -1,49 +1,57 @@
 from django.db import models
 from product.models import Product
 from customers.models import Customer
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from core.models import BaseModel
+from django.utils import timezone
 
 
-class Receipt(BaseModel):
-    order_details = models.TextField(default='No information')
+class Coupon(BaseModel):
+    code = models.CharField(max_length=30, unique=True)
+    valid_since = models.DateTimeField()
+    valid_until = models.DateTimeField()
+    discount = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(90)])
 
     def __str__(self):
-        return self.order_details[:120]
+        return self.code
+
+    def is_coupon_valid(self):
+        now = timezone.now()
+        if self.valid_until < now or self.valid_since > now:
+            return False
+        return True
 
 
 class Order(BaseModel):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
-    paid = models.BooleanField(default=False)
-    discount = models.PositiveIntegerField(blank=True, null=True, default=None)
-    receipt = models.OneToOneField(Receipt, on_delete=models.CASCADE, related_name='rorder', null=True, blank=True)
+    coupon = models.OneToOneField(Coupon, on_delete=models.CASCADE, related_name='coupon_order', null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    discount = models.PositiveIntegerField(blank=True, null=True, default=0, editable=False)
+    city = models.CharField(max_length=20, default='Not specified')
+    body = models.CharField(max_length=120, default='Not specified')
+    postal_code = models.CharField(
+        max_length=10,
+        default='XXXXXXXXXX',
+        validators=[RegexValidator(r'\d{10}', message='Invalid Postal code')])
+    STATUS = [('pending', 'PENDING'), ('checking', 'CHECKING'), ('sending', 'SENDING'), ('done', 'DONE')]
+    status = models.CharField(max_length=30, choices=STATUS, default='PENDING')
+    transaction_code = models.CharField(max_length=20, null=True, editable=False)
 
     class Meta:
-        ordering = ('paid', '-updated')
+        ordering = ('is_paid', '-updated')
 
     def __str__(self):
         return f'{self.customer} - {self.id}'
 
-    def get_total_price(self):
+    def apply_coupon(self):
+        self.discount = self.coupon.discount if self.coupon else 0
+
+    def get_total_price(self, code):
         total = sum(item.get_cost() for item in self.items.all())
         if self.discount:
             discount_price = total * self.discount / 100
             return int(total - discount_price)
         return total
-
-    def get_details(self):
-        items = self.items.all()
-        result = ''
-        for item in items:
-            result += item.get_item_details() + '\n'
-        return result
-
-    def create_receipt(self):
-        if self.paid:
-            self.receipt = Receipt.objects.create(order_details=self.get_details())
-            print(self.receipt.order_details)
-            return self.receipt.id
-        return False
 
 
 class OrderItem(BaseModel):
@@ -51,6 +59,8 @@ class OrderItem(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField(default=1)
+    STATUS = [('ok', 'OK'), ('no', 'NO'), ('supplying', 'SUPPLYING')]
+    status = models.CharField(max_length=30, choices=STATUS, default='OK')
 
     def __str__(self):
         return str(self.id)
@@ -58,18 +68,7 @@ class OrderItem(BaseModel):
     def get_cost(self):
         return self.price * self.quantity
 
-    def get_item_details(self):
-        detail = f'{self.product}|{self.quantity}|{self.price}|{self.get_cost()}'
-        return detail
+    def check_status(self):
+        return True if self.status == 'OK' else False
 
 
-class Coupon(BaseModel):
-    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True, related_name='used_coupon')
-    code = models.CharField(max_length=30, unique=True)
-    valid_since = models.DateTimeField()
-    valid_until = models.DateTimeField()
-    discount = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(90)])
-    active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.code
