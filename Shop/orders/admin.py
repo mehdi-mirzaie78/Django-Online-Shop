5b3d9425bd3fd0f36ae729182f557073559f8a62
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-10 20:19:38 +0330

    Shop | orders | admin.py | renamed fields

--- Shop/orders/admin.py
+++ Shop/orders/admin.py
@@ -10,12 +10,12 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Coupon)
 class CouponAdmin(BaseAdmin):
-    list_display = ('discount', 'valid_since', 'valid_until', 'active')
+    list_display = ('discount', 'valid_since', 'valid_until', 'is_active')
 
 
 @admin.register(Order)
 class OrderAdmin(BaseAdmin):
-    list_display = ('id', 'customer', 'paid')
-    list_filter = ('paid', 'customer')
+    list_display = ('id', 'customer', 'is_paid')
+    list_filter = ('is_paid', 'customer')
     search_fields = ('customer',)
     inlines = (OrderItemInline,)
