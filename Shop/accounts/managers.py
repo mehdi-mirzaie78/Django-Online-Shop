4330d5311047e07de17e8dac892f4fe5f0c127ca
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-10 13:11:31 +0330

    Shop | accounts | managers.py | Added normalize_phone_number

--- Shop/accounts/managers.py
+++ Shop/accounts/managers.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import BaseUserManager
+from django.core.validators import ValidationError
+import re
 
 
 class UserManager(BaseUserManager):
@@ -13,7 +15,9 @@ class UserManager(BaseUserManager):
         if not full_name:
             raise ValueError('user must have a full name')
 
-        user = self.model(phone_number=phone_number, email=self.normalize_email(email), full_name=full_name)
+        user = self.model(phone_number=self.normalize_phone_number(phone_number),
+                          email=self.normalize_email(email),
+                          full_name=full_name)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -23,3 +27,9 @@ class UserManager(BaseUserManager):
         user.is_admin = True
         user.save(using=self._db)
         return user
+
+    def normalize_phone_number(self, phone_number, pattern=r'^(\+989|09)+\d{9}$'):
+        valid_phone = re.compile(pattern)
+        if not valid_phone.match(phone_number):
+            raise ValidationError("Phone number can be one of these forms: +989XXXXXXXXX | 09XXXXXXXXX")
+        return phone_number
