ec33a5ac1f7b0a4199180732baa52c57270dbb79
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-10 13:33:02 +0330

    Shop | product | views.py | Used get_active_list in views

--- Shop/product/views.py
+++ Shop/product/views.py
@@ -5,12 +5,12 @@ from .models import Product
 
 class HomeView(View):
     def get(self, request):
-        products = Product.objects.all()
+        products = Product.objects.get_active_list().all()  # gets all objects that are active
         return render(request, 'product/index.html', {'products': products})
 
 
 class ProductDetailsView(View):
     def get(self, request, slug):
-        product = Product.objects.get(slug=slug)
+        product = Product.objects.get_active_list().get(slug=slug)
         properties = product.properties.all()
         return render(request, 'product/details.html', {'product': product, 'properties': properties})
