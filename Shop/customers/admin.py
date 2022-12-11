c6966a1161d584d747deb80db1f7db2989bd6b35
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-12 01:07:04 +0330

    Shop | customers | admin.py | Customized Admin panel

--- Shop/customers/admin.py
+++ Shop/customers/admin.py
@@ -3,30 +3,30 @@ from .models import Customer, Address
 from core.admin import BaseAdmin
 
 
-class AddressInline(admin.TabularInline):
+class AddressInline(admin.StackedInline):
     model = Address
+    extra = 0
 
 
 @admin.register(Customer)
 class CustomerAdmin(BaseAdmin):
     ordering = ('user',)
-    list_display = ('user',)
-    search_fields = ('user_full_name', 'user_phone_number', 'user_email')
+    list_display = ('user', 'user_full_name', 'phone_number')
+    search_fields = ('user__full_name', 'user__phone_number')
 
     fieldsets = (
-        ('Personal Information', {'fields': ('user',)}),
-        ('Optional', {'fields': ('image',)})
+        ('Personal Information', {'fields': ('user', 'gender', 'age',)}),
+        ('Optional', {'fields': ('image_tag', 'image')}),
     )
 
+    readonly_fields = ('image_tag',)
     inlines = (AddressInline,)
+    list_per_page = 10
 
 
 @admin.register(Address)
 class AddressAdmin(BaseAdmin):
     ordering = ('city',)
-    list_display = ('customer', 'city', 'body', 'postal_code')
-    list_editable = ('city', 'body')
+    list_display = ('customer', 'city', 'postal_code')
     list_filter = ('city', 'body')
-
-# admin.site.register(Customer, CustomerAdmin)
-# admin.site.register(Address, AddressAdmin)
+    search_fields = ('customer', 'city', 'body')
