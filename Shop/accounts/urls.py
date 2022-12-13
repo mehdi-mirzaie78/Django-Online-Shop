ec4f64737f9df5fe7d4f4fe2a83ae425b32cb2e1
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-13 19:33:39 +0330

    Shop | accounts | urls.py | updated profile url

--- Shop/accounts/urls.py
+++ Shop/accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('verify/', views.UserRegisterVerifyCodeView.as_view(), name='verify_code'),
     path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
-    path('profile/<int:user_id>/', views.UserProfileView.as_view(), name='user_profile'),
+    path('profile/', views.UserProfileView.as_view(), name='user_profile'),
 
 
 ]
