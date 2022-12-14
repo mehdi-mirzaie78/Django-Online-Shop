c0bcbe0b9570f674a5c56506aecc09e35a91ea98
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-14 18:20:02 +0330

    Shop | product | forms.py | Added new AddToCartForm for shopping

--- Shop/product/forms.py
+++ Shop/product/forms.py
@@ -6,3 +6,8 @@ class ProductSearchForm(forms.Form):
         max_length=100,
         required=False, label='',
         widget=forms.TextInput(attrs={'class': 'form-control col-12 py-1', 'placeholder': 'Search'}))
+
+
+class AddToCartForm(forms.Form):
+    number = forms.IntegerField(min_value=1, max_value=3, label='',
+                                widget=forms.NumberInput(attrs={'class': 'form-control'}))
