53dfb17c4476d4c50936ec9ed34e1ac6b7af68df
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-14 04:22:35 +0330

    Shop | accounts | urls.py | Added reset password urls

--- Shop/accounts/urls.py
+++ Shop/accounts/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
     path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
+    path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('confirm/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 
 ]
