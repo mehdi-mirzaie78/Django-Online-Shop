aa7d009b2b1361fbc0a25498dac9b461c12840d6
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-14 04:21:37 +0330

    Shop | product | urls.py | Added category filter url

--- Shop/product/urls.py
+++ Shop/product/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'product'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('category/<slug:category_slug>/', views.HomeView.as_view(), name='category_filter'),
     path('details/<slug:slug>/', views.ProductDetailsView.as_view(), name='product_details'),
 ]
