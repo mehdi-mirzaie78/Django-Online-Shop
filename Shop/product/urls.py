24256047ee519774aa6cd4740d552c970e490e1b
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-14 18:24:01 +0330

    Shop | product | urls.py | Added url of landing view & changed path for home url

--- Shop/product/urls.py
+++ Shop/product/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'product'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.LandingPageView.as_view(), name='landing'),
+    path('home/', views.HomeView.as_view(), name='home'),
     path('category/<slug:category_slug>/', views.HomeView.as_view(), name='category_filter'),
     path('details/<slug:slug>/', views.ProductDetailsView.as_view(), name='product_details'),
 ]
