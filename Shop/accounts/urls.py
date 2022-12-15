83e6ee0b21deaf62bcbd38c2256820ea4c3587b1
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-15 14:25:27 +0330

    Shop | accounts | urls.py | Added UserClearImageView url

--- Shop/accounts/urls.py
+++ Shop/accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
     path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('profile/clear_image/', views.UserClearImageView.as_view(), name='user_profile_clear_image'),
     path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
     path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
