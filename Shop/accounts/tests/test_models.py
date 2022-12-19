c05a904035f399008b8594dc1a8a68fb8eb5a69c
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-19 18:43:47 +0330

    Shop | accounts | test_models | Updated

--- Shop/accounts/tests/test_models.py
+++ Shop/accounts/tests/test_models.py
@@ -8,14 +8,15 @@ class TestUser(TestCase):
         self.user = User.objects.create_user(
             email='sample@gmail.com',
             phone_number='09123456789',
-            full_name='sample',
+            full_name='sample sample',
             password='abc'
         )
+        self.user.groups.create(name='sample')
 
     def test_user_creation(self):
         self.assertEqual(self.user.email, 'sample@gmail.com')
         self.assertEqual(self.user.phone_number, '09123456789')
-        self.assertEqual(self.user.full_name, 'sample')
+        self.assertEqual(self.user.full_name, 'sample sample')
         self.assertEqual(self.user.is_active, True)
         self.assertEqual(self.user.is_admin, False)
         self.assertEqual(self.user.is_staff, False)
@@ -27,11 +28,8 @@ class TestUser(TestCase):
     def test_user_str(self):
         self.assertEqual(str(self.user), self.user.email)
 
-    def test_user_has_perm(self):
-        self.assertEqual(self.user.has_perm('core.add_user'), True)
-
-    def test_user_has_module_perms(self):
-        self.assertEqual(self.user.has_module_perms('core'), True)
+    def test_role(self):
+        self.assertEqual(str(self.user.role()), 'sample')
 
 
 class TestOtpCode(TestCase):
@@ -44,6 +42,15 @@ class TestOtpCode(TestCase):
     def test_otp_creation(self):
         self.assertEqual(self.otp.phone_number, '09123456789')
         self.assertEqual(self.otp.code, 1234)
+
+    def test_otp_is_code_available(self):
+        self.assertEqual(OtpCode.is_code_available('09123456789'), True)
+        for i in range(5):
+            OtpCode.objects.create(
+                phone_number='09123456789',
+                code=1234,
+            )
+        self.assertEqual(OtpCode.is_code_available('09123456789'), False)
 
     def test_otp_str(self):
         self.assertEqual(str(self.otp), f'{self.otp.phone_number} - {self.otp.code} - {self.otp.created}')
