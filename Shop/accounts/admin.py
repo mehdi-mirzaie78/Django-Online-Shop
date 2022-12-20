1059c89728e43774109d19ea8628022aea1dacea
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-21 03:15:05 +0330

    Shop | accounts | admin.py | Nothing

--- Shop/accounts/admin.py
+++ Shop/accounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .forms import UserCreationForm, UserChangeForm
 from .models import User, OtpCode
-from django.contrib.auth.models import Group
 
 
 @admin.register(OtpCode)
