7e12306808186b779e787b39837e305e550f5daa
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-20 15:38:22 +0330

    Shop | Shop | celery_conf.py | Created celery_conf.py to use arvan cloud buckets and more

---
+++ Shop/Shop/celery_conf.py
@@ -0,0 +1,18 @@
+from celery import Celery
+from datetime import timedelta
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shop.settings')
+
+celery_app = Celery('Shop')
+celery_app.autodiscover_tasks()
+
+# Celery Configs
+celery_app.conf.broker_url = 'amqp://rabbitmq'
+celery_app.conf.result_backend = 'rpc://'
+celery_app.conf.task_serializer = 'json'
+celery_app.conf.result_serializer = 'pickle'
+celery_app.conf.accept_content = ['json', 'pickle']
+celery_app.conf.result_expires = timedelta(days=1)
+celery_app.conf.task_always_eager = False
+celery_app.conf.worker_prefetch_multiplier = 4
