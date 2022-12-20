b1742a6d817d6386545c34cba74d873ad13ba070
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-12-21 01:50:46 +0330

    Shop | product | tasks.py | Created tasks.py for using celery to manage the methods

---
+++ Shop/product/tasks.py
@@ -0,0 +1,23 @@
+from bucket import bucket
+from celery import shared_task
+
+
+# TODO: Can be async?
+def all_objects_task():
+    result = bucket.get_objects()
+    return result
+
+
+@shared_task
+def delete_object_task(key):
+    bucket.delete_object(key)
+
+
+@shared_task
+def download_object_task(key):
+    bucket.download_object(key)
+
+
+@shared_task()
+def upload_object_task(key):
+    bucket.upload_object(key)
