1e24b156e9ce6bee87d9ed34bb0ba8eedf7f4b8d
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2022-12-30 03:14:20 +0330

    Shop | orders | cart.py | Created Cart class for saving user's shopping cart into session and having the functionality that we need

---
+++ Shop/orders/cart.py
@@ -0,0 +1,49 @@
+from product.models import Product
+
+CART_SESSION_ID = 'cart'
+
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(CART_SESSION_ID)
+        if not cart:
+            cart = self.session[CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.get_active_list().filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+            cart[str(product.id)]['price_no_discount'] = product.price_no_discount
+            cart[str(product.id)]['image'] = product.image
+
+        for item in cart.values():
+            item['total_price'] = int(item['price']) * item['quantity']
+            item['discount_price'] = item['price_no_discount'] - int(item['price'])
+
+            yield item
+
+    def __len__(self):
+        return len([item for item in self])
+
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        self.cart[product_id]['quantity'] = quantity
+        self.save()
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def save(self):
+        self.session.modified = True
+
+    def get_total_price(self):
+        return sum(item['total_price'] for item in self)
