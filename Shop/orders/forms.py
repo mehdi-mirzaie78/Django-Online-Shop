6e16075b20f0af76a787d0e2609a139134ea20d6
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2022-12-31 00:03:22 +0330

    Shop | orders | forms.py | Updated

--- Shop/orders/forms.py
+++ Shop/orders/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from core.utils import phone_regex_validator
 
 
 class AddToCartForm(forms.Form):
@@ -11,11 +12,23 @@ class ChooseAddressForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
+        self.order = kwargs.pop('order', None)
         super(ChooseAddressForm, self).__init__(*args, **kwargs)
+
         self.customer = self.request.user.customer
         queryset = self.customer.addresses.all()
-        self.fields['address_id'].queryset = queryset
-        self.fields['address_id'].initial = queryset.first()
+        self.fields['address'].queryset = queryset
+        order_address = queryset.filter(postal_code=self.order.postal_code)
 
-    address_id = forms.ModelChoiceField(queryset=None, label=_('Address'), initial=None,
-                                        widget=forms.Select(attrs={'class': 'form-control'}))
+        if order_address.exists():
+            self.fields['address'].initial = order_address.get()
+
+        if self.order.phone_number:
+            self.fields['phone_number'].initial = self.order.phone_number
+        else:
+            self.fields['phone_number'].initial = self.customer.user.phone_number
+    address = forms.ModelChoiceField(queryset=None, label=_('Address'), initial=None,
+                                     widget=forms.Select(attrs={'class': 'form-control'}))
+
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),
+                                   validators=[phone_regex_validator])
