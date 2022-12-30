8c19fe1326aa0c7761986aa67d120a5c7b1278d8
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2022-12-30 23:57:37 +0330

    Shop | orders | views.py | Updated OrderDetailView, Created OrderDeleteView, PaymentView

--- Shop/orders/views.py
+++ Shop/orders/views.py
@@ -7,6 +7,7 @@ from .forms import AddToCartForm, ChooseAddressForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order, OrderItem
 from customers.models import Customer, Address
+from django.utils.translation import gettext_lazy as _
 
 
 class CartView(View):
@@ -51,19 +52,46 @@ class OrderCreateView(LoginRequiredMixin, View):
 
 class OrderDetailView(LoginRequiredMixin, View):
     form_class = ChooseAddressForm
+    template_name = 'orders/order_details.html'
+
+    def setup(self, request, *args, **kwargs):
+        self.order = get_object_or_404(Order, id=kwargs['order_id'])
+        return super(OrderDetailView, self).setup(request, *args, **kwargs)
 
     def get(self, request, order_id):
-        order = get_object_or_404(Order, id=order_id)
-        if address_id := request.GET.get('address_id', None):
-            address = get_object_or_404(Address, id=address_id)
+        order = self.order
+        form = self.form_class(request=request, order=order)
+        if order.check_address():
+            return render(request, self.template_name, {'order': order, 'form': form})
+        return redirect('customers:address_create')
+
+    def post(self, request, order_id):
+        order = self.order
+        form = self.form_class(request.POST, request=request, order=order)
+        if form.is_valid():
+            address = form.cleaned_data['address']
+            phone_number = form.cleaned_data['phone_number']
+
             order.city = address.city
             order.body = address.body
             order.postal_code = address.postal_code
+            order.phone_number = phone_number
             order.save()
-
-        if order.check_address():
-            return render(request, 'orders/order.html', {'order': order, 'form': self.form_class(request=request)})
-        return redirect('orders:address_create')
+            return redirect('orders:order_details', order.id)
+        return render(request, self.template_name, {'order': order, 'form': form})
 
 
+class OrderDeleteView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        order.delete()
+        messages.success(request, _('Order deleted successfully'), 'info')
+        return redirect('accounts:user_profile')
 
+
+class PaymentView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        order.is_paid = True
+        order.save()
+        return redirect('accounts:user_profile')
