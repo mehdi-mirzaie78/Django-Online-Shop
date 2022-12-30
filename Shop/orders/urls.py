aa6a8adee78e98a957b72af12d3a15fc11b58cd9
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2022-12-30 23:59:51 +0330

    Shop | orders | urls.py | Added delete, payment urls

--- Shop/orders/urls.py
+++ Shop/orders/urls.py
@@ -5,6 +5,9 @@ app_name = 'orders'
 urlpatterns = [
     path('create/', views.OrderCreateView.as_view(), name='order_create'),
     path('detail/<int:order_id>/', views.OrderDetailView.as_view(), name='order_details'),
+    path('delete/<int:order_id>/', views.OrderDeleteView.as_view(), name='order_delete'),
+    path('payment/<int:order_id>/', views.PaymentView.as_view(), name='payment'),
+
     path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
