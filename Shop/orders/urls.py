5fa0ea8c67ae1d42f794eca21c31a24fab3d9fab
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2023-01-02 01:13:48 +0330

    Shop | orders | urls.py | Commented on urls

--- Shop/orders/urls.py
+++ Shop/orders/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 app_name = 'orders'
 urlpatterns = [
+    # Orders
     path('create/', views.OrderCreateView.as_view(), name='order_create'),
-    path('detail/<int:order_id>/', views.OrderDetailView.as_view(), name='order_details'),
+    path('udpate/<int:order_id>/', views.OrderUpdateView.as_view(), name='order_update'),
+    path('details/<int:order_id>/', views.OrderDetailView.as_view(), name='order_details'),
     path('delete/<int:order_id>/', views.OrderDeleteView.as_view(), name='order_delete'),
     path('payment/<int:order_id>/', views.PaymentView.as_view(), name='payment'),
-
+    # Shopping Cart
     path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
