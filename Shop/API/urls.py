3d9e37da819aee9641b4999f03bd82e2ef90429b
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2023-01-03 17:17:57 +0330

    Shop | API | urls.py | Added product, bucket urls

--- Shop/API/urls.py
+++ Shop/API/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('profile/update/', views.ProfileUpdateAPIView.as_view(), name='api_profile_update'),
     path('addresses/', views.AddressListAPIView.as_view(), name='api_addresses_list'),
     path('address/create/', views.AddressCreateAPIView.as_view(), name='api_address_create'),
-
+    path('products/', views.ProductListAPIView.as_view(), name='api_products_list'),
+    path('product/<slug:slug>/', views.ProductDetailAPIView.as_view(), name='api_product_detail'),
+    path('bucket/', views.BucketListAPIView.as_view(), name='api_bucket_list'),
+    path('bucket/delete/<str:key>/', views.BucketDeleteAPIView.as_view(), name='api_bucket_delete'),
+    path('bucket/download/<str:key>/', views.BucketDownloadAPIView.as_view(), name='api_bucket_download'),
+    path('bucket/upload/<str:key>', views.BucketUploadAPIView.as_view(), name='api_bucket_upload'),
 
 ]
