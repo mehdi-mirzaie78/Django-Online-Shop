2897d1d6160608a8d585f91ee3b37f6cab30975b
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2023-01-03 13:03:02 +0330

    Shop | API | urls.py | Added profile update & address list, create urls

--- Shop/API/urls.py
+++ Shop/API/urls.py
@@ -3,5 +3,10 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('profile/', views.ProfileAPIView.as_view(), name='api_profile')
+    path('profile/', views.ProfileAPIView.as_view(), name='api_profile'),
+    path('profile/update/', views.ProfileUpdateAPIView.as_view(), name='api_profile_update'),
+    path('addresses/', views.AddressListAPIView.as_view(), name='api_addresses_list'),
+    path('address/create/', views.AddressCreateAPIView.as_view(), name='api_address_create'),
+
+
 ]
