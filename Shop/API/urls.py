275112b6e2d7b98811c135191d5d3eb11b18c529
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2023-01-04 21:47:04 +0330

    Shop | API | urls.py | Added Cart urls

--- Shop/API/urls.py
+++ Shop/API/urls.py
@@ -3,10 +3,13 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
+    # --------------------- accounts app ---------------------
     path('profile/', views.ProfileAPIView.as_view(), name='api_profile'),
     path('profile/update/', views.ProfileUpdateAPIView.as_view(), name='api_profile_update'),
+    # --------------------- customers app --------------------
     path('addresses/', views.AddressListAPIView.as_view(), name='api_addresses_list'),
     path('address/create/', views.AddressCreateAPIView.as_view(), name='api_address_create'),
+    # --------------------- product app ----------------------
     path('categories/', views.CategoryListAPIView.as_view(), name='api_categories_list'),
     path('products/', views.ProductListAPIView.as_view(), name='api_products_list'),
     path('product/<slug:slug>/', views.ProductDetailAPIView.as_view(), name='api_product_detail'),
@@ -14,5 +17,8 @@ urlpatterns = [
     path('bucket/delete/<str:key>/', views.BucketDeleteAPIView.as_view(), name='api_bucket_delete'),
     path('bucket/download/<str:key>/', views.BucketDownloadAPIView.as_view(), name='api_bucket_download'),
     path('bucket/upload/<str:key>', views.BucketUploadAPIView.as_view(), name='api_bucket_upload'),
-
+    # --------------------- order app ------------------------
+    path('cart/', views.CartAPIView.as_view(), name='api_cart'),
+    path('cart/add/<int:product_id>/', views.CartAPIView.as_view(), name='api_cart_add'),
+    path('cart/remove/<int:product_id>/', views.CartAPIView.as_view(), name='api_cart_remove')
 ]
