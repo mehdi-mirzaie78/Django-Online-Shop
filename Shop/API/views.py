ae677c2eae8166a7ac2c71b0ef45352d6ede8458
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2023-01-04 21:48:24 +0330

    Shop | API | views.py | Created CartAPIView with get, post, delete

--- Shop/API/views.py
+++ Shop/API/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from orders.cart import Cart
 from product import tasks
 from bucket import bucket
 from product.models import Product, Category
 from .serializers import ProfileSerializer, ProfileUpdateSerializer, AddressSerializer, ProductSerializer, \
-    ProductDetailSerializer, CategorySerializer
+    ProductDetailSerializer, CategorySerializer, OrderSerializer, \
+    CustomerUnpaidOrdersSerializer, QuantitySerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, status
 
@@ -65,7 +68,7 @@ class CategoryListAPIView(generics.ListAPIView):
 class ProductListAPIView(APIView):
 
     def get(self, request):
-        products = Product.objects.all()
+        products = Product.objects.filter(is_available=True)
         serializer = ProductSerializer(instance=products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -102,3 +105,33 @@ class BucketUploadAPIView(APIView):
     def get(self, request, key):
         tasks.upload_object_task.delay(key)
         return Response(status=status.HTTP_200_OK, data={'message': 'object uploaded'})
+
+
+# --------------------- cart ---------------------
+
+class CartAPIView(APIView):
+
+    def get(self, request):
+        cart = Cart(request)
+        result = cart.cart.copy()
+        if request.user.is_authenticated:
+            customer = request.user.customer
+            serializer = CustomerUnpaidOrdersSerializer(instance=customer)
+            result.update(serializer.data)
+        return Response(result, status=status.HTTP_200_OK)
+
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        serializer = QuantitySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if cart.add(product=product, quantity=serializer.validated_data['quantity']):
+            return Response(data=cart.cart, status=status.HTTP_200_OK)
+        return Response(data={'message': 'product is not available'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        if cart.remove(product=product):
+            return Response(data={'message': 'product removed from cart'}, status=status.HTTP_200_OK)
+        return Response(data={'message': 'product is not in your cart'}, status=status.HTTP_400_BAD_REQUEST)
