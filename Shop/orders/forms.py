8477ae448e6cf44053ad49424c106c3736e9ad58
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2023-01-05 01:47:09 +0330

    Shop | orders | forms.py | Updated and added coupon field

--- Shop/orders/forms.py
+++ Shop/orders/forms.py
@@ -8,12 +8,12 @@ class AddToCartForm(forms.Form):
                                   widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
 
-class ChooseAddressForm(forms.Form):
+class ChooseAddressApplyCouponForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
         self.order = kwargs.pop('order', None)
-        super(ChooseAddressForm, self).__init__(*args, **kwargs)
+        super(ChooseAddressApplyCouponForm, self).__init__(*args, **kwargs)
 
         self.customer = self.request.user.customer
         queryset = self.customer.addresses.all()
@@ -27,8 +27,19 @@ class ChooseAddressForm(forms.Form):
             self.fields['phone_number'].initial = self.order.phone_number
         else:
             self.fields['phone_number'].initial = self.customer.user.phone_number
+
+        if self.order.coupon:
+            self.fields['coupon'].initial = self.order.coupon.code
+
     address = forms.ModelChoiceField(queryset=None, label=_('Address'), initial=None,
+                                     help_text=_(
+                                         'If you have an address choose it from the list. Otherwise, add a new one'),
                                      widget=forms.Select(attrs={'class': 'form-control'}))
 
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),
-                                   validators=[phone_regex_validator])
+    phone_number = forms.CharField(validators=[phone_regex_validator], label=_('Phone number'),
+                                   help_text=_('Enter your phone number. Example: +989123456789'),
+                                   widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    coupon = forms.CharField(required=False, label=_('Coupon'),
+                             help_text=_('If you have coupon, You can enter it here'),
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
