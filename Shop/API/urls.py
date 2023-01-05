163962faad085aba0214384174b184a7520054ee
Mehdi Mirzaie <mehdimirzaie1378@gmail.com>
2023-01-05 20:39:07 +0330

    Shop | API | urls.py | Added comment create url

--- Shop/API/urls.py
+++ Shop/API/urls.py
@@ -13,11 +13,13 @@ urlpatterns = [
     path('categories/', views.CategoryListAPIView.as_view(), name='api_categories_list'),
     path('products/', views.ProductListAPIView.as_view(), name='api_products_list'),
     path('product/<slug:slug>/', views.ProductDetailAPIView.as_view(), name='api_product_detail'),
+    path('product/<slug:slug>/comment/create/', views.CommentCreateAPIView.as_view(), name='api_comment_create'),
+    # --------------------- bucket -----------------------
     path('bucket/', views.BucketListAPIView.as_view(), name='api_bucket_list'),
     path('bucket/delete/<str:key>/', views.BucketDeleteAPIView.as_view(), name='api_bucket_delete'),
     path('bucket/download/<str:key>/', views.BucketDownloadAPIView.as_view(), name='api_bucket_download'),
     path('bucket/upload/<str:key>', views.BucketUploadAPIView.as_view(), name='api_bucket_upload'),
-    # --------------------- order app ------------------------
+    # --------------------- orders app ------------------------
     path('cart/', views.CartAPIView.as_view(), name='api_cart'),
     path('cart/add/<int:product_id>/', views.CartAPIView.as_view(), name='api_cart_add'),
     path('cart/remove/<int:product_id>/', views.CartAPIView.as_view(), name='api_cart_remove')
