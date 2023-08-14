7d1cd1a7d240f420d87f887f312b15a0ba5a2599
mehdi <mehdimirzaie1378@gmail.com>
2023-08-14 12:55:31 +0000

    fixed showing images debug False

--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -1,3 +1,6 @@
+from django.urls import re_path
+from django.views.static import serve
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -40,4 +43,8 @@ urlpatterns += i18n_patterns(
     path('customers/', include('customers.urls', namespace='customers')),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
+urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),]
+
+urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
